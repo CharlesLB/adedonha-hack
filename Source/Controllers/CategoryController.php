1bc10c306e889547b1fff81fc804aa9f7cb43d2a
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-08 13:19:30 -0300

    App

--- Source/Controllers/CategoryController.php
+++ Source/Controllers/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController
 {
     private $view;
 
-    public function __construct($router) 
+    public function __construct($router)
     {
         $this->view = Engine::create(__DIR__ . "/../../Views", "php");
         $this->view->addData(["router" => $router]);
@@ -29,34 +29,38 @@ class CategoryController
     {
         $categoryData = filter_var_array($data, FILTER_SANITIZE_STRING);
 
-        if (in_array("", $categoryData)){
-            $callback["message"] = message("Informe o nome da categoria a ser cadastrada", "error");
+        $category = new Category();
+        $category->name = $categoryData["name"];
+
+        if (!$category->validate()) {
+            $callback["message"] = message($category->fail()->getMessage(), "error");
             echo json_encode($callback);
             return;
         }
 
-        $category = new Category();
-        $category->name = $categoryData["name"];
         $category->save();
-
-        $callback["category"]= $this->view->render("admin/fragments/category", ["category" => $category]);
+        $callback["category"] = $this->view->render("admin/fragments/category", ["category" => $category]);
         $callback["message"] = message("Categoria cadastrada com sucesso!", "success");
         echo json_encode($callback);
     }
 
     public function delete(array $data): void
     {
-        if (empty($data["id"])){
+        if (empty($data["id"])) {
             return;
         }
 
         $id = filter_var($data["id"], FILTER_VALIDATE_INT);
         $category = (new Category())->findById($id);
-        if($category){
+        if ($category) {
             $category->destroy();
         }
 
         $callback["remove"] = true;
         echo json_encode($callback);
+    }
+
+    public function show(){
+        
     }
 }
--- Source/Models/Category.php
+++ Source/Models/Category.php
@@ -3,11 +3,28 @@
 namespace Source\Models;
 
 use CoffeeCode\DataLayer\DataLayer;
+use Exception;
 
 class Category extends DataLayer
 {
     public function __construct()
     {
         parent::__construct("categories", ["name"]);
+    }
+
+    public function validate(): bool
+    {
+        if (empty($this->name)){
+            $this->fail = new Exception("Insira o nome da categoria!");
+            return false;
+        }
+
+        $categoryByName = $this->find("name = :name", "name={$this->name}")->count();
+        if($categoryByName){
+            $this->fail = new Exception("Categoria já cadastrada!");
+            return false;
+        }
+            
+        return true;
     }
 }
