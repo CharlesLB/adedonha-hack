3db6656a49ac940764f4ace91d0f6f109943ab64
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-09 12:07:49 -0300

    Helpers Functions

--- Source/Boot/Helpers.php
+++ Source/Boot/Helpers.php
@@ -21,3 +21,17 @@ function message(string $message, string $type): string
 {
     return "<div class=\"message {$type}\">{$message}</div>";
 }
+
+//
+// ─── WORDS TREATMENT ────────────────────────────────────────────────────────────
+//
+
+function verifyWords(array $words, string $letter){
+    foreach ($words as $word){
+        if( strpos($word,0,1) == $letter){
+            return true;
+        }
+    }
+    
+    return false;
+}
