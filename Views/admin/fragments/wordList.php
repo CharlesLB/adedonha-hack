395537684bdfc8cdb0d80079395b5fcab0446497
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-09 12:08:10 -0300

    WordList Fragment

--- Views/admin/fragments/wordList.php
+++ Views/admin/fragments/wordList.php
@@ -1,12 +1,15 @@
 
 <section class="words">
     <?php
+    
     for($letterCode = ord("A"); $letterCode <= ord('Z'); $letterCode++){
         $letter = chr($letterCode);
-        if (!empty($words)) :
+        if (!empty($words) && verifyWords($words, $letter)) :
             echo "<span>{$letter}</span>";
             foreach ($words as $word) :
+                if (substr($word,0,1) == $letter):
                 $v->insert("admin/fragments/word", ["word" => $word]);
+                endif;
             endforeach;
         endif;
     }
