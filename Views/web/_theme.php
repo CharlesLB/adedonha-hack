ee9fbe911f757e7958f971d3c120910c568da014
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-12 13:00:55 -0300

    Interface

--- Views/web/_theme.php
+++ Views/web/_theme.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= url("/Views/web/assets/css/styles.css?l"); ?>" />
+    <link rel="stylesheet" href="<?= url("/Views/web/assets/css/styles.css?2"); ?>" />
     <title><?= $title; ?></title>
 </head>
 
--- Views/web/home.php
+++ Views/web/home.php
@@ -1,18 +1,21 @@
 <?php $v->layout('web/_theme'); ?>
 
-<div class="intro">
-        <div class="left">
+<header>
+    <div class="intro">
+        <div class="text">
             <h1>Bem Vindo ao Adedonha Hack</h1>
             <h2>Basta dizer as categorias e qual a letra que daremos todas as respostas</h2>
 
             <a href="#start">Começar a trapacear!</a>
         </div>
-        <div class="right">
-        </div>
+        <img id="logo" src="<?= url("Views/web/assets/img/header.svg"); ?>" />
     </div>
+</header>
 
-    <div class="vector" style="height: 150px; overflow: hidden;">
-        <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-            <path d="M-1.01,4.05 C132.73,171.80 315.01,-134.10 517.04,99.76 L602.26,199.44 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path>
-        </svg>
+<div class="wave">
+    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.99 C150.00,150.00 349.20,-49.99 500.00,49.99 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
 </div>
+
+<section id="start">
+    
+</section>
