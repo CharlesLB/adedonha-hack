16d41e2af2e58e349173a169fcbc4b73b17a772a
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-18 15:35:18 -0300

    Match Controller

--- Source/Controllers/Match.php
+++ Source/Controllers/Match.php
@@ -5,12 +5,13 @@ namespace Source\Controllers;
 use League\Plates\Engine;
 use Source\Models\Category;
 use Source\Models\Word;
-
+use Exception;
 
 
 class Match
 {
     private $view;
+    private $error;
 
     public function __construct($router)
     {
@@ -18,31 +19,68 @@ class Match
         $this->view->addData(["router" => $router]);
     }
 
-   
-    public function start( array $data): void
-    {
-        $data = filter_var_array($data, FILTER_SANITIZE_STRING);
-        $names = $data["category"];
-
-        
-
-        foreach ($names as $name){
-            $category = new Category();
-
-            $categories[] = $category->show($name);
-
-            var_dump($categories);
-        }
-
-        echo $this->view->render("web/app", [
-            "title" => "App | " . SITE,
-            "categories" => $categories
-        ]);
-    }
-
     public function search(array $data): void
     {
+        echo json_encode($data);
         $data = filter_var_array($data, FILTER_SANITIZE_STRING);
 
+        if (!$this->validate($data)) {
+            $callback["message"] = message($this->error->getMessage(), "error");
+            echo json_encode($callback);
+            return;
+        }
+
+        $letter = strtoupper($data["letter"]);
+        $categories = $this->findCategories($data["categories"]);
+
+        foreach($categories as $category){
+            $answer = $this-answer($letter, $category);
+            $answers[] = $answer;
+        }
+    }
+
+    //
+    // ─── PRIVATE FUNCTIONS ──────────────────────────────────────────────────────────
+    //
+
+    private function validate(array $data): bool
+    {
+        if(!$data["categories"]===null){
+            $this->error = new Exception("Selecione as categorias");
+            return false;
+        }
+
+        if(!$data["letter"]){
+            $this->error = new Exception("Escolha uma letra");
+            return false;
+        }
+
+        $category = new Category;
+
+        foreach($data["categories"] as $name){
+            $category->name = $name;
+            if(!$category->matchValidade()){
+                $category->matchValidade();
+                $this->error = new Exception($category->fail);
+                return false;
+            }
+        }
+            
+        return true;
+    }
+
+    private function findCategories(array $data): array
+    {
+        foreach ($data as $name){
+            $category = new Category;
+            $categories[] = $category->show($name);
+        }
+
+        return $categories;
+    }
+
+    private function answer(string $letter, object $category): array
+    {
+        
     }
 }
