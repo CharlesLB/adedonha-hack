504d54b903e0dd6a26027bee1394dd1d29afaae5
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-19 11:36:21 -0300

    Web Template

--- Views/web/_theme.php
+++ Views/web/_theme.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= url("/Views/web/assets/css/styles.css?5546"); ?>" />
+    <link rel="stylesheet" href="<?= url("/Views/web/assets/css/styles.css"); ?>" />
     <link rel="stylesheet" href="<?= url("/shared/css/globals.css"); ?>">
     <link rel="icon" href="<?= url("/shared/img/icon.ico") ?> " type="image/x-icon" />
     <title><?= $title; ?></title>
